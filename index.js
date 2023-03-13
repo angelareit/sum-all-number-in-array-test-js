36840cdd2700bd9698de857d826ebdd844497180
angelareit <anreit01@email.com>
2023-03-13 04:44:37 +0000

    added recursive function

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+ /*  let currSum;
+  for (let num of array)
+  {
+    if(Array.isArray(num))
+    {
+      return sumItems(currSum + num);
+    }
+    currSum=+num;
+  }
+  return currSum; */
+
+  let totalSum = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      // Print out all it's items individually
+      totalSum += sumItems(item);
+    } else {
+      totalSum += item;
+    }
+  }
+  );
+  return totalSum;
+
 }
 
 module.exports = sumItems;
